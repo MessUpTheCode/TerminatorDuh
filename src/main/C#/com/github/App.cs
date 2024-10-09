6a5a2069c94b4ba3ad73960839c9e49bd7b3865c
vinh doan <vinh.doan@buildium.com>
2024-10-09 09:58:22 -0400

    vinh

---
+++ src/main/C#/com/github/App.cs
@@ -0,0 +1,22 @@
+﻿namespace DefaultNamespace;
+using System.Threading.Tasks;
+
+
+
+public class App
+{
+
+    public void Example()
+    {
+        
+    }
+    public bool IsThisReal()
+    {
+        return false;
+    }
+
+    public void AddItem(string item)
+    {
+        items.Add(items);
+    }
+}
